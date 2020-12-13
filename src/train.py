4ab2501681cdc74c51bb352bd45c2850220aef1b
triple3567 <ericvho888@gmail.com>
2020-12-13 17:02:40 -0500

    projuct structure adjustment

--- src/train.py
+++ src/train.py
@@ -2,19 +2,37 @@ import load
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing
+import pandas as pd
 
 #   use logistic regression to train model
 #   gets dataset from load.py
 #   returns trained model
 
-def train():
-  train_features, train_labels, test_features, test_labels = load.execute()
+def plot_loss(history):
+  plt.plot(history.history['loss'], label='loss')
+  plt.plot(history.history['val_loss'], label='val_loss')
+  plt.ylim([0, 10])
+  plt.xlabel('Epoch')
+  plt.ylabel('Error [MPG]')
+  plt.legend()
+  plt.grid(True)
+  plt.show()
+
+
+def execute(train_features, train_labels, test_features, test_labels):
 
   print(train_features)
   print(train_labels)
   print(test_features)
   print(test_labels)
 
+  
+
+
 
 if __name__ == '__main__':
-  train()
+  execute(load.execute())
